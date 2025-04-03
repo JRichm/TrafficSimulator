9fcc74c5290a536388c480a42c0769ec74f06efc
JRichm <jblackenmusic@gmail.com>
2025-04-03 00:11:44 -0600

    return colors

--- morecpp/core/color.h
+++ morecpp/core/color.h
@@ -4,13 +4,13 @@ class Color {
 public:
     unsigned char r, g, b, a;
 
-    Color(unsigned char r = 255, unsigned char g = 255, unsigned char b = 255, unsigned char a = 255);
+    Color(unsigned char r = 255, unsigned char g = 255, unsigned char b = 255, unsigned char a = 255)
+        : r(r), g(g), b(b), a(a) {}
 
-
-    static Color Red();
-    static Color Green();
-    static Color Blue();
-    static Color Yellow();
-    static Color Black();
-    static Color White();
+    static Color Red() { return Color(255, 0, 0); }
+    static Color Green() { return Color(0, 255, 0); }
+    static Color Blue() { return Color(0, 0, 255); }
+    static Color Yellow() { return Color(255, 255, 0); }
+    static Color Black() { return Color(0, 0, 0); }
+    static Color White() { return Color(255, 255, 255); }
 };
