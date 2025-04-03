34478040f1bca6b4beeccb9165de6174cdb38866
JRichm <jblackenmusic@gmail.com>
2025-04-03 00:12:33 -0600

    add function implementation

--- morecpp/road/highwayRamp.h
+++ morecpp/road/highwayRamp.h
@@ -26,16 +26,35 @@ private:
 
 
 public:
-	HighwayRamp(const std::string& id, const Vector3& pos, const Vector3& dim, float speedLimit, RampType type);
+	HighwayRamp(const std::string& id, const Vector3& pos, const Vector3& dim, float speedLimit, RampType type)
+	  :	RoadSegment(id, pos, dim, speedLimit),
+		type(type),
+		mergeStartDistance(0),
+		mergeEndDistance(0),
+		targetLane(0) {
+	}
 
-	void setMainRoad(std::shared_ptr<RoadSegment> road, float startDist, float endDist, int lane);
+	void setMainRoad(std::shared_ptr<RoadSegment> road, float startDist, float endDist, int lane) {
+		mainRoad = road;
+		mergeStartDistance = startDist;
+		mergeEndDistance = endDist;
+		targetLane = lane;
+	}
 
-	RampType getType() const;
-	std::weak_ptr<RoadSegment> getMainRoad() const;
-	MergeInfo getMergeParameters() const;
-	float getMergeAngle() const;
+	RampType getType() const { return type; }
+	std::weak_ptr<RoadSegment> getMainRoad() const { return mainRoad; }
 
-	int getTargetLane() const;
-	float getMergeStartDistance() const;
-	float getMergeEndDistance() const;
+	MergeInfo getMergeParameters() const {
+		MergeInfo info;
+		info.mergeStartDistance = mergeStartDistance;
+		info.mergeEndDistance = mergeEndDistance;
+		info.targetLane = targetLane;
+		info.mergeAngle = 15.0f;
+		return info;
+	}
+
+	float getMergeAngle() const { return 15.0f; }
+	int getTargetLane() const { return targetLane; }
+	float getMergeStartDistance() const { return mergeStartDistance; }
+	float getMergeEndDistance() const { return mergeEndDistance; }
 };
