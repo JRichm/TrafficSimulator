0414d7334909d1d4b504a2daa337287ea50ff138
JRichm <jblackenmusic@gmail.com>
2025-04-03 00:12:41 -0600

    add function implementation

--- morecpp/road/junction.h
+++ morecpp/road/junction.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <memory>
 #include <string>
-#include "vec3.h"
+#include "../core/vec3.h"
 
 
 // forward declaration
@@ -19,17 +19,40 @@ protected:
 
 
 public:
-	Junction(const std::string& id, const Vector3& pos);
+	Junction(const std::string& id, const Vector3& pos) : id(id), position(pos) {}
 	virtual ~Junction() = default;
 
-	void connectRoad(std::shared_ptr<RoadSegment> road);
-	std::vector<std::shared_ptr<RoadSegment>> getConnectedRoads() const;
+	void connectRoad(std::shared_ptr<RoadSegment> road) {
+		connectedRoads.push_back(road);
+	}
 
-	virtual std::vector<std::shared_ptr<RoadSegment>> getExitRoads(std::shared_ptr<RoadSegment> entryRoad) const;
+	std::vector<std::shared_ptr<RoadSegment>> getConnectedRoads() const {
+		std::vector<std::shared_ptr<RoadSegment>> roads;
+		for (const auto& weakRoad : connectedRoads) {
+			if (auto road = weakRoad.lock()) {
+				roads.push_back(road);
+			}
+		}
+		return roads;
+	}
+
+	virtual std::vector<std::shared_ptr<RoadSegment>> getExitRoads(std::shared_ptr<RoadSegment> entryRoad) const {
+		std::vector<std::shared_ptr<RoadSegment>> exits;
+		for (const auto& weakRoad : connectedRoads) {
+			if (auto road = weakRoad.lock()) {
+				if (road != entryRoad) {
+					exits.push_back(road);
+				}
+			}
+		}
+		return exits;
+	}
+
 	virtual bool canNavigate(std::shared_ptr<RoadSegment> fromRoad, std::shared_ptr<RoadSegment> toRoad, Vehicle* vehicle) = 0;
 
-	virtual void update(float deltaTime);
 
-	const std::string& getId() const;
-	const Vector3& getPosition() const;
+	virtual void update(float deltaTime) {}
+
+	const std::string& getId() const { return id; }
+	const Vector3& getPosition() const { return position; }
 };
