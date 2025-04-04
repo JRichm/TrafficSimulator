b341f39715ed28db6f5b71266e3c66ade5b44e41
JRichm <jblackenmusic@gmail.com>
2025-04-04 00:55:54 -0600

    update shader loader to load shaders from file path instead of source

--- morecpp/util/shader_loader.h
+++ morecpp/util/shader_loader.h
@@ -1,12 +1,46 @@
 #pragma once
 
-#include <glad/glad.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <glad/glad.h>
 
 
 class ShaderLoader {
 public:
-	static GLuint loaderShaders(const char* vertSource, const char* fragSource) {
+	static GLuint loadShaders(const char* vertexPath, const char* fragmentPath) {
+		std::string vertexCode;
+		std::string fragmentCode;
+		std::ifstream vShaderFile;
+		std::ifstream fShaderFile;
+
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try {
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+
+			std::stringstream vShaderStream, fShaderStream;
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+
+			vShaderFile.close();
+			fShaderFile.close();
+
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		} catch (std::ifstream::failure& e) {
+			std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+			return 0;
+		}
+
+		const char* vertSource = vertexCode.c_str();
+		const char* fragSource = fragmentCode.c_str();
+
+
 		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 		glShaderSource(vertexShader, 1, &vertSource, NULL);
 		glCompileShader(vertexShader);
