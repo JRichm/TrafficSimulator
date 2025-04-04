d7f28b8c4127c26dfee1042de0c5ef561cac151e
JRichm <jblackenmusic@gmail.com>
2025-04-04 00:59:11 -0600

    implement openGLDisplay

--- morecpp/ui/openglDisplay.h
+++ morecpp/ui/openglDisplay.h
@@ -3,7 +3,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include "roadNetwork.h"
+
+#include "../road/roadSegment.h"
 
 
 class OpenGLDisplay {
@@ -12,32 +13,40 @@ private:
 	GLuint shaderProgram;
 	GLuint VAO, VBO;
 
-	// camera
+	// camera settings
 	glm::vec3 cameraPos;
 	glm::vec3 cameraTarget;
 	glm::vec3 cameraUp;
+	float orthographicLeft = -120.0f;
+	float orthographicRight = 120.0f;
+	float orthographicBottom = -60.0f;
+	float orthographicTop = 60.0f;
+
+	// view settings
 	float zoom;
+	static OpenGLDisplay* currentInstance;
 
 	// setup
 	void setupRectangleVertices();
-	void setupShaders();
 
 	// drawing
-	void drawRoadSegment(const RoadSegment& road);
-	void drawJunction(const Junction& junction);
-	void drawVehicle(const Vehicle& vehicle);
+	void drawRectangle(const glm::vec3& position, const glm::vec3& scale, const glm::vec3& color);
+
+
+	static void scrollCallBack(GLFWwindow* window, double xoffset, double yoffset);
+	void processScroll(double yoffset);
 
 
 public:
-	OpenGLDisplay(int width = 800, int height = 600);
+	OpenGLDisplay(int width = 1280, int height = 720);
 	~OpenGLDisplay();
 
 	bool isOpen() const;
 	bool processEvents();
-	void render(const RoadNetwork& network);
+	void render(const RoadSegment& network);
 
-	// camera controls
-	void moveCamera(const glm::vec3& offset);
-	void zoomCamera(float factor);
-	void setCameraTarget(const glm::vec3& target);
+	// // camera controls
+	// void moveCamera(const glm::vec3& offset);
+	// void zoomCamera(float factor);
+	// void setCameraTarget(const glm::vec3& target);
 };
