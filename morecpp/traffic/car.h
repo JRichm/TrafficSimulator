eadeb1c2adf29737098e7af24921de8eae282eb6
JRichm <jblackenmusic@gmail.com>
2025-04-04 00:54:08 -0600

    remove override declarations

--- morecpp/traffic/car.h
+++ morecpp/traffic/car.h
@@ -1,20 +1,11 @@
 #pragma once
 
 #include "vehicle.h"
-#include "highwayRamp.h"
 
 class Car : public Vehicle {
-private:
-	bool isAutonomous;
-	
-
 public:
-	Car(const Vector3& pos, const Vector3& dim, const Color& col, float preferredSpeed = 5.0f);
-
-	void adjustSpeedForTraffic(const std::vector<std::shared_ptr<Vehicle>>& nearbyCars, float deltaTime) override;
-	bool shouldChangeLane(const std::vector<std::shared_ptr<Vehicle>>& nearbyCars) override;
-	void handleRamp(HighwayRamp* ramp, float deltaTime) override;
-
-	void setAutonomous(bool autonomous);
-	bool isAutonomousDriving() const; // not sure about this one 
+    Car(const Vector3& position, const Vector3& dimensions, const Color& color, float speed)
+        : Vehicle(VehicleType::CAR, position, dimensions, color) {
+        setPreferredSpeed(speed);
+    }
 };
