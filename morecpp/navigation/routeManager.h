a63eedd9e02ac7ad0689c14d3ad590eddcf62307
JRichm <jblackenmusic@gmail.com>
2025-04-05 11:29:10 -0600

    remove findPath function

--- morecpp/navigation/routeManager.h
+++ morecpp/navigation/routeManager.h
@@ -23,25 +23,4 @@ public:
     void addRoadSegment(std::shared_ptr<RoadSegment> roadSegment) {
         roadSegments[roadSegment->getId()] = roadSegment;
     }
-
-    // Simplified placeholder for path finding
-    // In a real implementation, you'd use A* or Dijkstra's algorithm
-    std::vector<std::shared_ptr<RoadSegment>> findPath(
-        std::shared_ptr<RoadSegment> currentRoad,
-        float distanceAlongRoad,
-        std::shared_ptr<Destination> destination) {
-
-        std::vector<std::shared_ptr<RoadSegment>> path;
-
-        // For now, just return a path with the current road and destination road
-        if (currentRoad) {
-            path.push_back(currentRoad);
-        }
-
-        if (destination && destination->getConnectedRoad() != currentRoad) {
-            path.push_back(destination->getConnectedRoad());
-        }
-
-        return path;
-    }
 };
