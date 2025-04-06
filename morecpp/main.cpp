f9ddc255bce4b3b1b230d690c508701d49aa9cc2
JRichm <jblackenmusic@gmail.com>
2025-04-06 13:33:54 -0600

    remove unnecessary includes

--- morecpp/main.cpp
+++ morecpp/main.cpp
@@ -1,20 +1,8 @@
-#include <random>
 #include <iostream>
-
-#include "core/vec3.h"
-#include "road/roadSegment.h"
-#include "road/roadNetwork.h"
-#include "road/simpleJunction.h"
-#include "road/spawnPoint.h"
-#include "navigation/destination.h"
-#include "traffic/car.h"
 #include "ui/openGLGameLoop.h"
 
 int main() {
-    RoadNetwork roadNetwork;
-    roadNetwork.buildNetwork(2, 2, 400.0f, 32.0f, 10.0f);
-
-    OpenGLGameLoop::runNetwork(roadNetwork);
+    OpenGLGameLoop::runCustomNetworkSimulation();
 
     return 0;
 }
