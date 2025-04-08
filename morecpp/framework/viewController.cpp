05241cdc890777dc65dd19c819bd1646c5db233b
JRichm <jblackenmusic@gmail.com>
2025-04-08 00:18:53 -0600

    fix shaderLoader include file name from shader_loader

--- morecpp/framework/viewController.cpp
+++ morecpp/framework/viewController.cpp
@@ -5,7 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "../shaders/shader_loader.h"
+#include "../shaders/shaderLoader.h"
 #include "../traffic/vehicle.h"
 
 
